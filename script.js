1583d37bf6fe8cde82305e48d7fc51db95538bca
jdisla07 <71913937+jdisla07@users.noreply.github.com>
2021-10-01 00:05:00 -0400

    added styling and js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-const x = "";
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
+
+open.addEventListener('click', ()=> container.classList.add('show-nav'))
+close.addEventListener('click', ()=> container.classList.remove('show-nav'))
 
 
